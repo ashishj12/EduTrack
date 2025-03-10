997a275a7bf2af4be64753cb3baba9d8830b5986
Tanishksaini <tanishksaini51@gmail.com>
2025-03-10 21:01:29 +0530

    "App Removed"

--- App/app.config.js
+++
@@ -1,8 +0,0 @@
-// app.config.js
-const withKotlinVersion = require('./withKotlinVersion');
-
-export default ({ config }) => {
-  // Apply the custom plugin to modify the native Gradle file
-  config = withKotlinVersion(config);
-  return config;
-};
